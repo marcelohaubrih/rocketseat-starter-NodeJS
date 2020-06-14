656fb36ad7f9302a130dade44cd4c9b98a5d54f9
Marcelo Haubrih <marcelohaubrih@gmail.com>
2020-06-14 17:43:12 -0300

    Criando tratativa de erros na API

--- server.js
+++ server.js
@@ -1,9 +1,54 @@
 const express = require('express');
+const mongoose = require('mongoose');
+const requireDir = require('require-dir');
+const cors = require('cors');
 
+
+var whitelist = ['http://exemple.com']
+var corsOptions = {
+  origin: function (origin, callback) {
+      //console.log(origin);
+    if (whitelist.indexOf(origin) !== -1) {
+      callback(null, true)
+    } else {
+      callback(new Error('Not allowed by CORS'))
+    }
+  }
+}
+
+
+//Iniciando o APP
 const app = express();
+app.use(express.json());
 
-app.get('/', (req, res) => {
-    res.send("Hello");
+//app.use((req, res, next) => {
+//    console.log('Acessou o Midller');
+//    res.header('Access-Control-Allow-Origin',"http://exemplo.com")
+    app.use(cors());
+//    next();
+//});
+//
+
+//Iniciando o DB
+mongoose.connect(
+    'mongodb://10.0.0.66:27017/nodeapi', 
+    { 
+        useNewUrlParser: true, 
+        useUnifiedTopology: true,
+        useCreateIndex: true,
+        useFindAndModify: false 
+    }
+).then(() => {
+    console.log("Conexão com MongoDB realizada com sucesso!");
+}).catch((erro) => {
+    console.log("Erro: Conexão com MongoDB não foi realizada com sucesso!");
 });
+
+
+requireDir('./src/models');
+
+
+// Rotas
+app.use('/api', require('./src/routes'));
 
 app.listen(3001);
---
+++ src/controllers/ProductController.js
@@ -0,0 +1,86 @@
+const mongoose = require('mongoose');
+const Product = mongoose.model('Product');
+
+module.exports = {
+    async index(req, res){
+        const { page = 1 } = req.query;
+        if (page === '0' || page === ''){
+            return res.status(400).json({
+                error: true,
+                message: "Erro: página nula ou zero!"
+            });
+        }else{
+            await Product.paginate({}, {page, limit:10}).then((product) => {
+                console.log(product.pages);
+                if(page <= product.pages){
+                    return res.json(product);
+                }else{
+                    return res.status(400).json({
+                        error: true,
+                        message: "Erro: página fora do escopo!"
+                    });                    
+                }
+            }).catch((erro) => {
+                return res.status(400).json({
+                    error: true,
+                    message: "Erro ao listar os produtos"
+                });
+            });
+        }
+    },
+
+    async show(req, res){
+        await Product.findById(req.params.id).then((product) => {
+            return res.json(product);
+        }).catch((erro) => {
+            return res.status(400).json({
+                error: true,
+                message: "Produto não encontrado!"
+            });
+        });
+    },
+
+    async store(req, res){
+        const product = await Product.create(req.body, (err) => {
+            if(err) return res.status(400).json({
+                error: true,
+                message: "Erro: produto não cadastrado!"
+            });
+            
+            return res.status(200).json({
+                error: false,
+                message: "Produto cadastrado com sucesso",
+                dados: product
+            });
+        });
+        
+    },
+
+    async update(req, res){
+        const product = await Product.findByIdAndUpdate(req.params.id, req.body, { new: true }, (err) => {
+            if(err) return res.status(400).json({
+                error: true,
+                message: "Error: produto não foi editado!"
+            });
+    
+            return res.json({
+                error: false,
+                message: "Produto editado com sucesso!"
+            });            
+        });
+        return res.json(product);
+    },
+
+    async destroy(req, res){
+        const product = await Product.findByIdAndRemove(req.params.id, (err) =>{
+            if(err) return res.status(400).json({
+                error: true,
+                message: "Erro: não foi possível apagar o registro"
+            });
+            return res.json({
+                error: false,
+                message: "Registro excluído com sucesso"
+            })
+        });
+    }
+};
